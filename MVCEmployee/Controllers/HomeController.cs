b4099e7e7c4247b629d388e4dc3a9daf164b0f2e
vokitoki <woja12@gmail.com>
2023-06-15 21:57:41 +0200

    Add project files.

---
+++ MVCEmployee/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCEmployee.Models;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Xml;
+
+namespace MVCEmployee.Controllers
+{
+    //Controller class.
+    public class HomeController : Controller
+    {
+        //Creating of list and count of employees.
+        public static List<Employee> List1 = new List<Employee> ();
+        public static int count=0;
+        
+        public ActionResult Index()
+        {
+            ViewBag.List = List1;
+            return View();
+        }
+        public ActionResult Add()
+        {            
+            ViewBag.List = List1;
+            return View();
+        }
+        public ActionResult Remove()
+        { 
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        //Handling of HTTP POST methods employee manipulation.
+        [HttpPost]
+        
+        public ActionResult Add(Models.Employee employee)
+        {
+            //Counting menbers with count.
+            employee.ID = count;
+            count++;
+            List1.Add(employee);
+            return View("Employees", employee);
+        }
+        //Handling removing of employee via POST HTTP method.
+        [HttpPost]
+        public ActionResult Remove(int ID)
+        {            
+            try {
+                List1.RemoveAll(e => e.ID == ID);
+            }
+            catch {
+                return View("Failure");
+            }
+            return View("Successful");
+        }
+        //Handlig unpdating of employee via POST HTTP method.
+        [HttpPost]
+        public ActionResult Update(Models.Employee employee)
+        {
+            
+            foreach(Employee i in List1)
+            {
+            if (employee.ID>count)
+            {
+                return View("Failure");     
+            }
+                if(employee.ID == i.ID)
+                {
+                    i.Name = employee.Name;
+                    i.Email= employee.Email;
+                    i.Phone = employee.Phone;
+                    break;
+                }
+            }
+            return View("Successful");
+        }
+
+    }
+}
---
+++ MVCEmployee/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCEmployee.Models
+{
+    //Model for Employees
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
